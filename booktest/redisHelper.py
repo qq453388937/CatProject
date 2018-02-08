c54f71ca1084cf41c7cbea4ce2049219165bd623
faith <453388937@qq.com>
2018-02-08 15:11:30 +0800

    立项2

--- booktest/redisHelper.py
+++ booktest/redisHelper.py
@@ -1,18 +1,19 @@
 # -*- coding:utf-8 -*-
 import redis
+from rediscluster import *
 import time
 
 
 class RedisHelper():
     def __init__(self, host='localhost', port=6379):
         self.__redis = redis.StrictRedis(host, port)
-
+    
     def get(self, key):
         if self.__redis.exists(key):
             return self.__redis.get(key)
         else:
             return ""
-
+    
     def set(self, key, value):
         """
 
@@ -22,7 +23,7 @@ class RedisHelper():
         """
         ret = self.__redis.set(key, value)
         return ret
-
+    
     def delete(self, key):
         self.__redis.delete(key)
 
@@ -45,7 +46,29 @@ def main():
     pipe.execute()  # 一次缓冲多条命令一次执行
     # r.set("mmd","666")
     print("ok")
+    
+    
+def cluster():
+    try:
+        # 构建所有的节点，Redis会使⽤CRC16算法，将键和值写到某个节点上
+        startup_nodes = [
+            {'host': '192.168.26.128', 'port': '7000'},
+            {'host': '192.168.26.130', 'port': '7003'},
+            {'host': '192.168.26.128', 'port': '7001'},
+        ]
+        # 构建StrictRedisCluster对象
+        src = StrictRedisCluster(startup_nodes=startup_nodes, decode_responses=True)
+        # 设置键为name、值为itheima的数据
+        result = src.set('name', 'itheima')
+        print(result)
+        # 获取键为name
+        name = src.get('name')
+        print(name)
+    except Exception as e:
+        print(e)
 
 
 if __name__ == '__main__':
     main()
+    
+    
